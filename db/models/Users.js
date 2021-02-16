c6a85c58a52f4850726dcb5ed79d102fe1c297ed
jflukac <juanfranciscolukac@gmail.com>
2021-02-16 13:22:34 -0300

    users model

---
+++ db/models/Users.js
@@ -0,0 +1,42 @@
+module.exports = (sequelize , dataTypes) =>{
+
+    let alias = 'Users';
+    let cols = {
+        id : {
+            autoIncrement : true,
+            primaryKey : true,
+            type : dataTypes.INTEGER,
+            allowNull : false
+        },
+        email : {
+            type : dataTypes.STRING,
+            allowNull : false,
+            unique: true
+        },
+        password: {
+            type: dataTypes.STRING,
+            allowNull: false
+        },
+        created_at: {
+            type: dataTypes.DATE,
+            allowNull: false
+        },
+        updated_at: {
+            type: dataTypes.DATE,
+        },
+        deleted_at: {
+            type: dataTypes.DATE,
+        }
+    }
+    let config = {
+        tableName: 'users',
+        timestamps: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at',
+        paranoid: true,
+    }
+    const Users = sequelize.define(alias, cols, config);
+
+    return Users;
+} 
