fc87b3dc3fa96c2733c7131de5e93699c1115d63
jflukac <juanfranciscolukac@gmail.com>
2021-02-18 18:22:46 -0300

    edit product validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -8,5 +8,11 @@ module.exports = {
         check('title').isLength({min:3, max:100}).withMessage('El nombre debe tener mas de 3 caracteres y menos de 100'),
         check('description').isLength({max:1000}).withMessage('La descripción debe ser menor a 1000 caracteres'),
         check('price').isInt({gt:0}).withMessage('El precio debe ser mayor a cero')
+    ],
+    productEdit: [
+        check('brand').isInt().withMessage('Debes elegir una marca'),
+        check('title').isLength({min:3, max:100}).withMessage('El nombre debe tener mas de 3 caracteres y menos de 100'),
+        check('description').isLength({max:1000}).withMessage('La descripción debe ser menor a 1000 caracteres'),
+        check('price').isInt({gt:0}).withMessage('El precio debe ser mayor a cero')
     ]
 }
