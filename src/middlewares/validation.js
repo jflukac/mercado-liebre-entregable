ae241d5b8b093bba120456cedf472b40fc1fbfc2
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 10:27:30 -0300

    register validation

--- src/middlewares/validation.js
+++ src/middlewares/validation.js
@@ -14,5 +14,9 @@ module.exports = {
         check('title').isLength({min:3, max:100}).withMessage('El nombre debe tener mas de 3 caracteres y menos de 100'),
         check('description').isLength({max:1000}).withMessage('La descripción debe ser menor a 1000 caracteres'),
         check('price').isInt({gt:0}).withMessage('El precio debe ser mayor a cero')
-    ]
+    ],
+    userRegister: [
+        check('email').isEmail().withMessage('El email ingresado no es valido'),
+        check('password').isLength({min:6, max:9}).withMessage('La contraseña debe tener de 6 a 9 caracteres')
+    ],
 }
