f7d887af9057374a987af063ef26395e1974c73d
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 17:15:01 -0300

    Search logic

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -11,10 +11,28 @@ const controller = {
 			//return res.send(products)
 			return res.render('index', {products})
 		})
-		.catch( error => { res.send(error)})
+		.catch( error => { 
+			console.log(error)
+			throw new Error('Error al acceder a la base de datos')
+		})
 	},
 	search: (req, res) => {
-		res.render('results')
+		db.Products.findAll({
+			where: {
+			  title: { [Op.like]: '%'+ req.query.keywords + '%'}
+			},
+			order: [
+			  ['created_at', 'DESC']
+			],
+			limit: 20,		
+		})
+		.then(products => {
+			res.render('results', {products, keywords: req.query.keywords})
+		})
+		.catch( error => { 
+			console.log(error)
+			throw new Error('Error al acceder a la base de datos')
+		})
 	},
 };
 
