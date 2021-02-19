f30c35305855273baadd336b6bcd450eaee0ec39
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 12:21:45 -0300

    Save User without login in

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 const path = require('path')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const bcrypt = require('bcrypt')
 
 /*Database configuration */
 const { Sequelize } = require('../db/models');
@@ -18,7 +19,18 @@ const controller = {
         res.render('./users/register', {errors:[], userAttempt:{}})
     },
     saveUser: (req, res) => {
-        
+        let errors = validationResult(req)
+		if (errors.isEmpty()) {
+            db.Users.create({
+                email: req.body.email,
+                password: bcrypt.hashSync(req.body.password, 10)
+            })
+            .then(user => {
+                return res.render('./users/profile', {user})
+            })
+        } else {
+            return res.render('./users/register', {errors:errors.mapped(), userAttempt:{...req.body}})
+        }
     },
     profile: (req, res) => {
         
