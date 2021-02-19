c64166196bf4b63aaad8ef61558bb3776a5d5db2
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 12:50:39 -0300

    register validates existing users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -10,27 +10,60 @@ const Op = Sequelize.Op
 
 const controller = {
     login: (req, res) => {
-        res.render('./users/login', {errors:[], userAttempt:{}})
+        res.render('./users/login', {userAttempt:{}})
     },
     loginProcess: (req, res) => {
-        
+        let errors = validationResult(req)
+		if (errors.isEmpty()) {
+
+        } else {
+            return res.render('./users/login', {errors:errors.mapped(), userAttempt:{...req.body}})
+        }
     },
     register: (req, res) => {
         res.render('./users/register', {errors:[], userAttempt:{}})
     },
     saveUser: (req, res) => {
-        let errors = validationResult(req)
-		if (errors.isEmpty()) {
-            db.Users.create({
-                email: req.body.email,
-                password: bcrypt.hashSync(req.body.password, 10)
-            })
-            .then(user => {
-                return res.render('./users/profile', {user})
-            })
-        } else {
-            return res.render('./users/register', {errors:errors.mapped(), userAttempt:{...req.body}})
-        }
+        db.Users.findOne({
+            where: {
+                email: req.body.email
+            }
+        })
+        .then(registerUser => {
+            if(registerUser) {
+                let errors= {
+                    userRegister : {
+                        msg: 'El usuario ya existe',
+                        param: 'userRegister',
+                        location: 'body'
+                    }
+                    
+                }
+                return res.render('./users/register', {errors:errors, userAttempt:{...req.body}})
+            } else {
+                let errors = validationResult(req)
+                //return res.send(errors)
+                if (errors.isEmpty()) {
+                    db.Users.create({
+                        email: req.body.email,
+                        password: bcrypt.hashSync(req.body.password, 10)
+                    })
+                    .then(user => {
+                        return res.render('./users/login', {userAttempt: user})
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        throw new Error('Error al acceder a la base de datos')
+                    })
+                } else {
+                    return res.render('./users/register', {errors:errors.mapped(), userAttempt:{...req.body}})
+                }
+            }
+        })
+        .catch(error => {
+            console.log(error)
+            throw new Error('Error al acceder a la base de datos')
+        }) 
     },
     profile: (req, res) => {
         
