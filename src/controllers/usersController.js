eaa62e556e57e0f8f5d1ace545ce6cec1a69ca03
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 10:26:39 -0300

    GET login page

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -8,7 +8,32 @@ const db = require('../db/models');
 const Op = Sequelize.Op
 
 const controller = {
-
+    login: (req, res) => {
+        
+        res.render('./users/login', {errors:[], userAttempt:{}})
+    },
+    loginProcess: (req, res) => {
+        
+    },
+    register: (req, res) => {
+    
+        res.render('./users/register', {errors:[], userAttempt:{}})
+    },
+    saveUser: (req, res) => {
+        
+    },
+    profile: (req, res) => {
+        
+    },
+    editUser: (req, res) => {
+        
+    },
+    avatar: (req, res) => {
+        
+    },
+    deleteUser: (req, res) => {
+        
+    }
 };
 
 module.exports = controller;
