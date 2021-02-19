a1e1481a370a6a7f9b68e2698643e8b60c263021
jflukac <juanfranciscolukac@gmail.com>
2021-02-19 18:09:52 -0300

    Searching Pages

--- src/routes/main.js
+++ src/routes/main.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.index); 
-router.get('/search', mainController.search); 
+router.get('/search/:page', mainController.search); 
 
 module.exports = router;
